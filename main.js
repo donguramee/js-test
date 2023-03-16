ef0e89305e3eca8cc0b5d4642771e4b6ee86566f
dongurame <sanulim94@gmail.com>
2023-03-17 03:40:11 +0900

    js study

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 let boxEl = document.querySelector(".box");
 
 console.log(boxEl);
+
+boxEl.addEventListener("click", function () {
+  console.log("Click!!");
+  boxEl.classList.add("active");
+  console.log(boxEl.classList.contains("active"));
+  boxEl.classList.remove("active");
+  console.log(boxEl.classList.contains("active"));
+});
