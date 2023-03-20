ecd5a67f7f086539752b0427c3075622b62676d3
dongurame <sanulim94@gmail.com>
2023-03-20 23:07:08 +0900

    js test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const boxEls = document.querySelectorAll(".box");
+const boxEl = document.querySelector(".box");
 
-boxEls.forEach(function (boxEl, index) {
-  boxEl.classList.add(`order-${index + 1}`);
-  console.log(index, boxEl);
-});
+console.log(boxEl.textContent);
+
+boxEl.textContent = "Rami";
+console.log(boxEl.textContent);
